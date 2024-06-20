d3c5b8ed50f01247e1b3f1474a84918df1c712ae
sele906 <seunga906@gmail.com>
2024-06-20 14:00:48 +0900

    commit

--- cosmetic/src/main/java/com/example/cosmetic/CosmeticApplication.java
+++ cosmetic/src/main/java/com/example/cosmetic/CosmeticApplication.java
@@ -8,16 +8,23 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 
 @SpringBootApplication
-public class CosmeticApplication {
+public class CosmeticApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CosmeticApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CosmeticApplication.class);
 	}
 
 	@Bean
